ccebb4e99a229994faf482cb485cf4a3281ae3d9
Erik Lintunen <erik.lintunen@network.rca.ac.uk>
2020-06-01 22:05:12 +0100

    change to build

--- static/webpack.common.js
+++ static/webpack.common.js
@@ -47,7 +47,7 @@ module.exports = {
       },
       {
 				test   : /\.(ttf|eot|woff(2)?)(\?[a-z0-9]+)?$/,
-				loader : 'file-loader?name=images/font/[hash].[ext]'
+				loader : 'file-loader',
 			},
     ],
   },
